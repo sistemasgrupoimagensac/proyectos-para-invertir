799a41572358e4827d1db9878b249f477a0047ec
sistemasgrupoimagensac2 <grupoimagen.908883889@gmail.com>
2024-12-17 11:29:16 -0500

    Like y dislike del proyecto

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\LikeController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/like',[LikeController::class, 'meInteresa']);
+Route::post('/dislike',[LikeController::class, 'noMeInteresa']);
