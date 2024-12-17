5e5f45544545ed5b2805e863e4a80c36cc6f6f08
Alejandro Rafael Chavez <jrafael@360creative.pe>
2024-12-17 11:22:22 -0500

    primeros endpoints

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('/projects')->group(function() {
+    Route::get('/', 'Api\AllProjectsController@getAll');
+    Route::get('/{id}', 'Api\ProjectDetailsController@getOne');
+});
